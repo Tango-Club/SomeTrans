15fb39d4905bccdc856032d3ec47f9a18a1b7d88
BiteTheDust <952130278@qq.com>
2021-06-01 10:17:57 +0800

    divide schema read

---
+++ SchemaReader.hpp
@@ -0,0 +1,114 @@
+#include <string>
+/*
+表定义类
+*/
+struct ColumnDefType
+{ //数据类型
+	std::string typeName;
+	std::vector<int> args;
+	ColumnDefType(const std::string &typeStr)
+	{
+		int pre = 0;
+		for (int i = 0; i < typeStr.length(); i++)
+		{
+			if (typeStr[i] == '(')
+				this->typeName = typeStr.substr(0, i), pre = i + 1;
+			else if (typeStr[i] == ',' || typeStr[i] == ')')
+			{
+				this->args.push_back(std::stoi(typeStr.substr(pre, i - pre)));
+				pre = i + 1;
+			}
+		}
+		std::cout << typeName << std::endl;
+		for (auto x : args)
+			std::cout << x << " ";
+		std::cout << std::endl;
+	}
+};
+struct ColumnInfo
+{ //列声明
+	std::string name;
+	int ordinal;
+	bool isUnsigned;
+	std::string charSet;
+	ColumnDefType columnDef;
+	int length;
+	int precision;
+	int scale;
+	ColumnInfo(const rapidjson::Document &doc) : columnDef(doc["ColumnDef"].GetString())
+	{
+		this->name = doc["Name"].GetString();
+		this->ordinal = doc["Ordinal"].GetInt();
+		this->isUnsigned = doc["Unsigned"].GetBool();
+		this->charSet = (doc["CharSet"].IsNull() ? "null" : doc["CharSet"].GetString());
+		this->length = (doc["Length"].IsNull() ? -1 : doc["Length"].GetInt());
+		this->precision = (doc["Precision"].IsNull() ? -1 : doc["Precision"].GetInt());
+		this->scale = (doc["Scale"].IsNull() ? -1 : doc["Scale"].GetInt());
+	}
+};
+struct IndexInfo
+{ //索引声明
+	//临时
+	std::string str;
+	IndexInfo(const std::string &indexStr)
+	{
+		this->str = indexStr;
+		std::cout << indexStr << std::endl;
+	}
+};
+struct PrimeKeyInfo
+{ //主键声明
+	//临时
+	std::string indexCol;
+	PrimeKeyInfo(const rapidjson::Document &doc)
+	{
+		this->indexCol = doc["IndexCols"].GetArray()[0].GetString();
+		std::cout << this->indexCol << std::endl;
+	}
+};
+struct TableInfo
+{ //表声明
+	std::string tableName;
+	std::string fromDataBase;
+	std::vector<ColumnInfo> columns;
+	std::vector<IndexInfo> indexs;
+	std::vector<PrimeKeyInfo> primeKeys;
+	TableInfo(std::ifstream &schemaInfo)
+	{
+		std::string tmp; //无用内容或空行
+		schemaInfo >> tmp >> this->fromDataBase >> tmp >> this->tableName;
+		int columnNums = 0;
+		schemaInfo >> tmp >> tmp >> columnNums;
+		std::getline(schemaInfo, tmp);
+		while (columnNums--)
+		{
+			std::string colStr;
+			std::getline(schemaInfo, colStr);
+			std::cout << colStr << std::endl;
+			rapidjson::Document doc;
+			doc.Parse(colStr.c_str());
+			columns.emplace_back(ColumnInfo(doc));
+		}
+		int indexNums = 0;
+		schemaInfo >> tmp >> tmp >> indexNums;
+		std::getline(schemaInfo, tmp);
+
+		while (indexNums--)
+		{
+			std::string colStr;
+			std::getline(schemaInfo, colStr);
+			indexs.emplace_back(IndexInfo(colStr));
+		}
+		int primeKeyNums = 0;
+		schemaInfo >> tmp >> tmp >> tmp >> primeKeyNums;
+		std::getline(schemaInfo, tmp);
+		while (primeKeyNums--)
+		{
+			std::string colStr;
+			std::getline(schemaInfo, colStr);
+			rapidjson::Document doc;
+			doc.Parse(colStr.c_str());
+			primeKeys.emplace_back(PrimeKeyInfo(doc));
+		}
+	}
+};
--- main.cpp
+++ main.cpp
@@ -12,6 +12,8 @@
 #include "rapidjson/stringbuffer.h"
 #include "rapidjson/writer.h"
 
+#include "SchemaReader.hpp"
+
 /**
  * @author dts，just for demo.
  * @author pxl, fuck demo.
@@ -24,120 +26,6 @@ const std::string SINK_FILE_DIR = "sink_file_dir";															// 输出文件
 const std::string SOURCE_FILE_NAME_TEMPLATE = "tianchi_dts_source_data_";									// 输入文件名，无需修改。
 const std::string SINK_FILE_NAME_TEMPLATE = "tianchi_dts_sink_data_";										// 输出文件名模板，无需修改。
 const std::string CHECK_TABLE_SETS = "customer,district,item,new_orders,order_line,orders,stock,warehouse"; // 待处理表集合，无需修改。
-
-/*
-表定义类
-*/
-struct ColumnDefType
-{ //数据类型
-	std::string typeName;
-	std::vector<int> args;
-	ColumnDefType(const std::string &typeStr)
-	{
-		int pre = 0;
-		for (int i = 0; i < typeStr.length(); i++)
-		{
-			if (typeStr[i] == '(')
-				this->typeName = typeStr.substr(0, i), pre = i + 1;
-			else if (typeStr[i] == ',' || typeStr[i] == ')')
-			{
-				this->args.push_back(std::stoi(typeStr.substr(pre, i - pre)));
-				pre = i + 1;
-			}
-		}
-		std::cout << typeName << std::endl;
-		for (auto x : args)
-			std::cout << x << " ";
-		std::cout << std::endl;
-	}
-};
-struct ColumnInfo
-{ //列声明
-	std::string name;
-	int ordinal;
-	bool isUnsigned;
-	std::string charSet;
-	ColumnDefType columnDef;
-	int length;
-	int precision;
-	int scale;
-	ColumnInfo(const rapidjson::Document &doc) : columnDef(doc["ColumnDef"].GetString())
-	{
-		this->name = doc["Name"].GetString();
-		this->ordinal = doc["Ordinal"].GetInt();
-		this->isUnsigned = doc["Unsigned"].GetBool();
-		this->charSet = (doc["CharSet"].IsNull() ? "null" : doc["CharSet"].GetString());
-		this->length = (doc["Length"].IsNull() ? -1 : doc["Length"].GetInt());
-		this->precision = (doc["Precision"].IsNull() ? -1 : doc["Precision"].GetInt());
-		this->scale = (doc["Scale"].IsNull() ? -1 : doc["Scale"].GetInt());
-	}
-};
-struct IndexInfo
-{ //索引声明
-	//临时
-	std::string str;
-	IndexInfo(const std::string &indexStr)
-	{
-		this->str = indexStr;
-		std::cout << indexStr << std::endl;
-	}
-};
-struct PrimeKeyInfo
-{ //主键声明
-	//临时
-	std::string indexCol;
-	PrimeKeyInfo(const rapidjson::Document &doc)
-	{
-		this->indexCol = doc["IndexCols"].GetArray()[0].GetString();
-		std::cout << this->indexCol << std::endl;
-	}
-};
-struct TableInfo
-{ //表声明
-	std::string tableName;
-	std::string fromDataBase;
-	std::vector<ColumnInfo> columns;
-	std::vector<IndexInfo> indexs;
-	std::vector<PrimeKeyInfo> primeKeys;
-	TableInfo(std::ifstream &schemaInfo)
-	{
-		std::string tmp; //无用内容或空行
-		schemaInfo >> tmp >> this->fromDataBase >> tmp >> this->tableName;
-		int columnNums = 0;
-		schemaInfo >> tmp >> tmp >> columnNums;
-		std::getline(schemaInfo, tmp);
-		while (columnNums--)
-		{
-			std::string colStr;
-			std::getline(schemaInfo, colStr);
-			std::cout << colStr << std::endl;
-			rapidjson::Document doc;
-			doc.Parse(colStr.c_str());
-			columns.emplace_back(ColumnInfo(doc));
-		}
-		int indexNums = 0;
-		schemaInfo >> tmp >> tmp >> indexNums;
-		std::getline(schemaInfo, tmp);
-
-		while (indexNums--)
-		{
-			std::string colStr;
-			std::getline(schemaInfo, colStr);
-			indexs.emplace_back(IndexInfo(colStr));
-		}
-		int primeKeyNums = 0;
-		schemaInfo >> tmp >> tmp >> tmp >> primeKeyNums;
-		std::getline(schemaInfo, tmp);
-		while (primeKeyNums--)
-		{
-			std::string colStr;
-			std::getline(schemaInfo, colStr);
-			rapidjson::Document doc;
-			doc.Parse(colStr.c_str());
-			primeKeys.emplace_back(PrimeKeyInfo(doc));
-		}
-	}
-};
 
 class Demo
 {
