a5f56b2a1430f74117645264da626ac8fed1ccad
te1t0ch1phead <caohongyi@live>
2021-06-16 10:40:14 +0800

    modify the start.sh

--- start.sh
+++ start.sh
@@ -11,13 +11,15 @@ echo "echo3: $3"
 echo "echo4: $4"
 APP_HOME=main.cpp
 OUT_NAME=main
-g++-11 $APP_HOME -std=c++20 -o $OUT_NAME  -O2 -pthread
+#g++-11 $APP_HOME -std=c++20 -o $OUT_NAME  -O2 -pthread
 echo "start $0 $1 $2 $3 $4 $@"
 nohup ./$OUT_NAME $@ & > ./lala.out
 
 for((i=0;i<100;i++))
 do
 sleep 1
+echo 'USER         PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND'
+ps -aux | grep  ./main
 free
 done
 
